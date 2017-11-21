38e6708a3439ffd63911f68555c5987567ea235a
Lizabeth Petersen <lizpetersen928@gmail.com>
2017-11-21 15:50:11 -0800

    finished building functions with jquery that add articles to DOM organized by date

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -9,12 +9,12 @@ let articles = [];
 
 function Article (rawDataObj) {
     // TODO: Use the JS object that is passed in to complete this constructor function:
-    this.title = title;
-    this.category = category;
-    this.author = author;
-    this.authorUrl = authorUrl;
-    this.publishedOn = publishedOn;
-    this.body = body;
+    this.title = rawDataObj.title;
+    this.category = rawDataObj.category;
+    this.author = rawDataObj.author;
+    this.authorUrl = rawDataObj.authorUrl;
+    this.publishedOn = rawDataObj.publishedOn;
+    this.body = rawDataObj.body;
     // Save ALL the properties of `rawDataObj` into `this`
 }
 
@@ -22,16 +22,16 @@ Article.prototype.toHtml = function() {
     // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
     // We do not have to recreate the article structure in the DOM each time.
 
-    let $newArticle = $('article.template').clone();
+    const $newArticle = $('article.template').clone();
     $newArticle.removeClass('template');
     /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
     if (!this.publishedOn) $newArticle.addClass('draft');
     $newArticle.attr('data-category', this.category);
-    $newArticle.find('a').text(this.author);
+    $newArticle.find('a').html(this.author);
     $newArticle.find('a').attr('href', this.authorUrl);
-    $newArticle.find('h1').text(this.title);
-    $newArticle.find('.article-body').text(this.body);
+    $newArticle.find('h1').html(this.title);
+    $newArticle.find('.article-body').html(this.body);
     $newArticle.attr('datetime', this.publishedOn);
 
     /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
@@ -43,7 +43,7 @@ Article.prototype.toHtml = function() {
       5. publication date. */
 
     // REVIEW: Display the date as a relative number of 'days ago'
-    $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+    $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000) + ' days ago');
     $newArticle.append('<hr>');
     return $newArticle;
 };
@@ -55,10 +55,10 @@ rawData.sort(function(a,b) {
 
 // TODO: Refactor these for loops using the .forEach() array method.
 
-for(let i = 0; i < rawData.length; i++) {
-    articles.push(new Article(rawData[i]));
-}
+rawData.forEach(function(articleData) {
+    articles.push(new Article(articleData));
+});
 
-for(let i = 0; i < articles.length; i++) {
-    $('#articles').append(articles[i].toHtml());
-}
+articles.forEach(function(articleData) {
+    $('#articles').append(articleData.toHtml());
+});
