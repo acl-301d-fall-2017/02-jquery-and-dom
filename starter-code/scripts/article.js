a0785253d98c5d401dfbf93f9d6b69a9bf483e55
Charlie Heiner <heinercj@gmail.com>
2017-11-21 14:25:01 -0800

    created cloned article in jQuery to use instances data

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -3,24 +3,36 @@
 let articles = [];
 
 // COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
-// PUT YOUR RESPONSE HERE
+// PUT YOUR RESPONSE HERE  Constructor function called Article. Make multiple instances of Articles object. 'This' is used to set properties of each instance with its own arguments.
+// rawDataObj is a parameter that holds an array of object literals that contains the properties and property values.
 
 function Article (rawDataObj) {
-  // TODO: Use the JS object that is passed in to complete this constructor function:
-  // Save ALL the properties of `rawDataObj` into `this`
+    // TODO: Use the JS object that is passed in to complete this constructor function:
+    // Save ALL the properties of `rawDataObj` into `this`
+    this.title = title;
+    this.category = category;
+    this.author = author;
+    this.authorUrl = authorUrl;
+    this.publishedOn = publishedOn;
+    this.body = body; 
+
 }
 
 Article.prototype.toHtml = function() {
-  // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
-  // PUT YOUR RESPONSE HERE
+    // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
+    // PUT YOUR RESPONSE HERE  We don't have to retype each article's html structure.  Since they are the same for each when can use the clone to poplute each instance. Keeping code DRY.
 
-  let $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+    let $newArticle = $('article.template').clone();
+    /* Done TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
-  if (!this.publishedOn) $newArticle.addClass('draft');
-  $newArticle.attr('data-category', this.category);
-
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
+    if (!this.publishedOn) $newArticle.addClass('draft');
+    $newArticle.attr('data-category', this.category);
+    $('address').html(this.author);
+    $('a').attr('href', this.authorUrl);
+    $('h1').html(this.title);
+    $('.article-body').html(this.body);
+    $('time').html(this.publishedOn);
+    /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
       1. author name,
       2. author url,
@@ -28,23 +40,23 @@ Article.prototype.toHtml = function() {
       4. article body, and
       5. publication date. */
 
-  // REVIEW: Display the date as a relative number of 'days ago'
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-  $newArticle.append('<hr>');
-  return $newArticle;
+    // REVIEW: Display the date as a relative number of 'days ago'
+    $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+    $newArticle.append('<hr>');
+    return $newArticle;
 };
 
 rawData.sort(function(a,b) {
-  // REVIEW: Take a look at this sort method; This may be the first time we've seen it. Look at the docs and think about how the dates would be sorted if the callback were not included in this method.
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    // REVIEW: Take a look at this sort method; This may be the first time we've seen it. Look at the docs and think about how the dates would be sorted if the callback were not included in this method.
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
 // TODO: Refactor these for loops using the .forEach() array method.
 
 for(let i = 0; i < rawData.length; i++) {
-  articles.push(new Article(rawData[i]));
+    articles.push(new Article(rawData[i]));
 }
 
 for(let i = 0; i < articles.length; i++) {
-  $('#articles').append(articles[i].toHtml());
+    $('#articles').append(articles[i].toHtml());
 }
