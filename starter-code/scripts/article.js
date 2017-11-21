42ee99d2f67a24245a176d5528413070617cc96d
Jack <lomax15@me.com>
2017-11-21 14:22:19 -0800

    tackled a few to-dos on article.js page

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -3,22 +3,27 @@
 let articles = [];
 
 // COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
-// PUT YOUR RESPONSE HERE
+// This function is creating a new instance of Article. - If it's a constructor function then the name is capitalized. - "This" refers to the instance of the article being created. - rawDataObj represents the article instance.
 
 function Article (rawDataObj) {
-  // TODO: Use the JS object that is passed in to complete this constructor function:
-  // Save ALL the properties of `rawDataObj` into `this`
+// TODO: Use the JS object that is passed in to complete this constructor function:
+// Save ALL the properties of `rawDataObj` into `this`
+    this.authorName = rawDataObj.authorName;
+    this.authorURL = rawDataObj.authorURL;
+    this.articleTitle = rawDataObj.articleTitle;
+    this.articleBody = rawDataObj.articleBody;
+    this.publishedOn = rawDataObj.publishedOn;
 }
 
 Article.prototype.toHtml = function() {
-  // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
-  // PUT YOUR RESPONSE HERE
+// COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
+// It is a very convenient way of copying elements on a page.
 
-  let $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+    let $newArticle = $('article.template').clone();
+    /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
-  if (!this.publishedOn) $newArticle.addClass('draft');
-  $newArticle.attr('data-category', this.category);
+    if (!this.publishedOn) $newArticle.addClass('draft');
+    $newArticle.attr('data-category', this.category);
 
   /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
